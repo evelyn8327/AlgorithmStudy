e73208844c1a576f9ddf2c5c359fc2d63df2c291
evelyn8327 <bokyungpark93@gmail.com>
2017-01-09 19:59:19 +0900

    %i

---
+++ 11816_octal_decimal_hexadecimal.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  11816_8_10_16_jinsu
+//
+//  Created by Valued customer on 1/3/17.
+//  Copyright © 2017 AI. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, const char * argv[]) {
+    
+    int n;
+    
+    scanf("%i",&n);
+    
+    
+    
+    
+    printf("%d\n",n);
+    
+    return 0;
+}
+
+
+
+
